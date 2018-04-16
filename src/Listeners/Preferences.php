0e690146aa44ac6d06a263f63c6b993521ce0dab
Charlie <charlie.kern01@gmail.com>
2018-04-16 14:54:27 +0000

    Apply fixes from StyleCI

--- src/Listeners/Preferences.php
+++ src/Listeners/Preferences.php
@@ -9,6 +9,7 @@
  * For the full copyright and license information, please view the license.md
  * file that was distributed with this source code.
  */
+
 namespace Reflar\NightMode\Listeners;
 
 use Flarum\Event\ConfigureUserPreferences;
