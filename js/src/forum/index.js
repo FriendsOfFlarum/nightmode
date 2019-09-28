1e2c5fc18b05c998cfe3d6e6379818ee01fa0bb5
Christian Lopez <Ralkage@live.com>
2019-09-27 22:49:59 -0400

    Fixes #15 a #16 and remove unused code

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,45 +1,46 @@
-import { extend, override } from 'flarum/extend';
-
+import {extend} from 'flarum/extend';
+import app from 'flarum/app';
 import SessionDropdown from 'flarum/components/SessionDropdown';
 import Button from 'flarum/components/Button';
 import Page from 'flarum/components/Page';
 import TagsPage from 'flarum/tags/components/TagsPage';
-import app from 'flarum/app';
 
 app.initializers.add('reflar-nightmode', app => {
-  extend(Page.prototype, 'init', function (vdom) {
-    if (app.session.user && app.session.user.preferences().reflarNightMode) {
-      $('body').addClass('dark');
-    } else {
-      $('body').removeClass('dark');
-    }
-  });
-
-  extend(TagsPage.prototype, 'config', function (vdom) {
-    if (app.session.user && app.session.user.preferences().reflarNightMode) {
-      $('body').addClass('dark');
-    } else {
-      $('body').removeClass('dark');
-    }
-  });
-
-  extend(SessionDropdown.prototype, 'items', function(items) {
-    let lightState = app.session.user.preferences().reflarNightMode == true ? false : true;
-
-    // Add night mode link to session dropdown
-    items.add(app.session.user && app.session.user.preferences().reflarNightMode ? 'nightmode' : 'daymode',
-      Button.component({
-        icon: lightState == true ? 'far fa-moon' : 'far fa-sun',
-        href: 'javascript:;',
-        children: lightState == true ? app.translator.trans('reflar-nightmode.forum.night') : app.translator.trans('reflar-nightmode.forum.day'),
-        onclick: function() {
-          // Toggle night mode on or off by changing the user preference
-          app.session.user.savePreferences({'reflarNightMode': lightState});
-
-          $('body').toggleClass('dark');
+    extend(Page.prototype, 'init', function () {
+        if (app.session.user && app.session.user.preferences().reflarNightMode) {
+            $('body').addClass('dark');
+        } else {
+            $('body').removeClass('dark');
         }
-      }),
-      -1
-    );
-  });
+    });
+
+    if (TagsPage) {
+        extend(TagsPage.prototype, 'config', function () {
+            if (app.session.user && app.session.user.preferences().reflarNightMode) {
+                $('body').addClass('dark');
+            } else {
+                $('body').removeClass('dark');
+            }
+        });
+    }
+
+    extend(SessionDropdown.prototype, 'items', function (items) {
+        let lightState = app.session.user.preferences().reflarNightMode == true ? false : true;
+
+        // Add night mode link to session dropdown
+        items.add(app.session.user && app.session.user.preferences().reflarNightMode ? 'nightmode' : 'daymode',
+            Button.component({
+                icon: lightState == true ? 'far fa-moon' : 'far fa-sun',
+                href: 'javascript:;',
+                children: lightState == true ? app.translator.trans('reflar-nightmode.forum.night') : app.translator.trans('reflar-nightmode.forum.day'),
+                onclick: function () {
+                    // Toggle night mode on or off by changing the user preference
+                    app.session.user.savePreferences({'reflarNightMode': lightState});
+
+                    $('body').toggleClass('dark');
+                }
+            }),
+            -1
+        );
+    });
 });
