43cd9652a9e38a841a6367f2689458f3d33c9b69
David Wheatley <davidwheatley03@gmail.com>
2020-06-10 07:58:34 +0100

    Remove unused permission code

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,8 +4,6 @@ import addSettingsItems from "./addSettingsItems";
 import setSelectedTheme from "./setSelectedTheme";
 
 app.initializers.add("fof-nightmode", (app) => {
-    // User.prototype.canUseDarkMode = Model.attribute("canUseDarkMode");
-
     addSettingsItems();
     setSelectedTheme();
 });
