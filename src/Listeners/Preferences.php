2d8a7734bd5f438ba4428c133c98f91c5daa0305
David Wheatley <davidwheatley03@gmail.com>
2020-06-17 18:43:46 +0100

    Update/add copyright notice

--- src/Listeners/Preferences.php
+++ src/Listeners/Preferences.php
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2020 FriendsOfFlarum.
  *
- * For the full copyright and license information, please view the license.md
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
