555bf966218ca3535646fc83205000bc229f66b1
David Wheatley <davidwheatley03@gmail.com>
2020-06-27 22:03:40 +0100

    Add default theme migration

---
+++ migrations/2020_06_27_000000_set_default_theme.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of fof/nightmode.
+ *
+ * Copyright (c) 2020 FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ */
+
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        /**
+         * @var \Flarum\Settings\SettingsRepositoryInterface
+         */
+        $settings = app('flarum.settings');
+
+        $settings->set('fof-nightmode.default_theme', '0');
+    },
+    'down' => function (Builder $schema) {
+        $settings = app('flarum.settings');
+
+        $settings->delete('fof-nightmode.default_theme');
+    },
+];
