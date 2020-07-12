52d565c7e1c7ca3e2e197b111a7947aa01724be2
David Sevilla Martin <me@datitisev.me>
2020-07-12 14:05:52 -0400

    Fix issue with retrieving default from JS app.data

--- js/src/common/config.js
+++ js/src/common/config.js
@@ -12,7 +12,7 @@ const Themes = {
     DARK: 2,
 
     // adds an "ultra default" of 0 if the admins don't set a default tsk tsk tsk
-    DEFAULT: () => Number.parseInt(app.data['fof-nightmode_default_theme']) || 0,
+    DEFAULT: () => Number.parseInt(app.data['fof-nightmode.default_theme']) || 0,
 };
 
 const Constants = {
