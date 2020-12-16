df766b31b1c24b89214af9b26108391e6dfd96d4
karaok491 <72854852+karaok491@users.noreply.github.com>
2020-12-16 10:35:42 +0000

    beta 15 (#43)
    * beta 15 prep
    
    * Apply fixes from StyleCI
    
    * Fix default mode preference
    
    * Apply fixes from StyleCI
    
    Co-authored-by: karaok491 <karaok491@users.noreply.github.com>

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,7 +1,31 @@
-import addSettingsModal from './addSettingsModal';
 import setSelectedTheme from '../common/setSelectedTheme';
+import Themes from '../common/Themes';
+
+function populateThemes() {
+    let options = {};
+
+    // add themes based on JS enum
+    Object.keys(Themes).forEach((theme, i) => {
+        if (theme === 'DEFAULT') return;
+
+        options[i] = app.translator.trans(`fof-nightmode.admin.settings.modal.theme_${theme.toLowerCase()}`);
+    });
+
+    return options;
+}
 
 app.initializers.add('fof-nightmode', () => {
-    addSettingsModal();
+    app.extensionData.for('fof-nightmode')
+    .registerSetting({
+        label: app.translator.trans('fof-nightmode.admin.settings.modal.default_theme'),
+        setting: 'fof-nightmode.default_theme',
+        type: 'select',
+        options: populateThemes()
+    })
+    .registerSetting({
+        label: app.translator.trans('fof-nightmode.admin.settings.modal.default_theme_helper'),
+        type: 'hidden',
+    });
+    
     setSelectedTheme();
 });
