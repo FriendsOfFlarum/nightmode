254afb91b325c330548e178189e8c8aea71bd147
Hậu Nguyễn <76046186+hadpro@users.noreply.github.com>
2020-12-23 12:15:36 +0700

    Fix error beta 15

--- extend.php
+++ extend.php
@@ -1,45 +1,39 @@
 <?php
 
-/*
- * This file is part of fof/nightmode.
- *
- * Copyright (c) 2020 FriendsOfFlarum.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace FoF\NightMode;
+namespace Flarum\NightMode;
 
 use Flarum\Extend;
 use Flarum\Foundation\Application;
-use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\Event\ConfigureUserPreferences;
 use FoF\Extend\Extend as FoFExtend;
+use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less')
         ->content(Content\HideBody::class),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less')
         ->content(Content\HideBody::class),
 
-    new Extend\Locales(__DIR__.'/resources/locale'),
+    new Extend\Locales(__DIR__ . '/locale'),
 
-    (new FoFExtend\ExtensionSettings())
+    (new FlarumExtend\ExtensionSettings())
         ->addKey('fof-nightmode.default_theme'),
 
-    function (Application $app) {
+    function (Application $app, Dispatcher $events) {
         $app->register(AssetsServiceProvider::class);
+
+        $events->listen(ConfigureUserPreferences::class, function (ConfigureUserPreferences $event) {
+            $event->add(
+                'fofNightMode',
+                'intval',
+                (int) app('flarum.settings')->get('fof-nightmode.default_theme', 0)
+            );
+
+            $event->add('fofNightMode_perDevice', 'boolval', false);
+        });
     },
-
-    (new Extend\User())
-        ->registerPreference('fofNightMode', function ($value) {
-            if ($value === '' || $value === null) {
-                $value = (int) app(SettingsRepositoryInterface::class)->get('fof-nightmode.default_theme', 0);
-            }
-
-            return (int) $value;
-        })
-        ->registerPreference('fofNightMode_perDevice', null, false),
 ];
