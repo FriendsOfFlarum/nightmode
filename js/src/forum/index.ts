7ae3693fcf85c1c89483427d8a5a08e2ded06864
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2022-07-29 22:05:18 +0200

    Exports common and helpers files

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,6 +2,8 @@ import app from 'flarum/forum/app';
 import addSettingsItems from './addSettingsItems';
 import setSelectedTheme from '../common/setSelectedTheme';
 
+export * from './extend';
+
 app.initializers.add('fof-nightmode', () => {
   addSettingsItems();
   setSelectedTheme();
