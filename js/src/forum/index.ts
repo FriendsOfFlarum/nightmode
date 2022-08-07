5b85c5aa116bcaf932eaa2a3f7d415d48b19ca63
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2022-08-07 12:43:54 +0200

    chore: export JavaScript for extensions to extend (#64)

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,6 +2,10 @@ import app from 'flarum/forum/app';
 import addSettingsItems from './addSettingsItems';
 import setSelectedTheme from '../common/setSelectedTheme';
 
+export * as perDeviceSetting from './helpers/perDeviceSetting';
+export * as switchTheme from './helpers/switchTheme';
+export { default as getTheme } from './getTheme';
+
 app.initializers.add('fof-nightmode', () => {
   addSettingsItems();
   setSelectedTheme();
